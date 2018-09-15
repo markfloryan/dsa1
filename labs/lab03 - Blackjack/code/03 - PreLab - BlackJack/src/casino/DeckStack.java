d12d0947ccb337197d90a75f5e97ee2851704b52
Mark Floryan <floryan@floryan.local>
2018-09-15 05:44:30 -0400

    whoops

--- labs/lab03 - Blackjack/code/03 - PreLab - BlackJack/src/casino/DeckStack.java
+++ labs/lab03 - Blackjack/code/03 - PreLab - BlackJack/src/casino/DeckStack.java
@@ -5,33 +5,21 @@ package casino;
  *
  */
 public class DeckStack {
-
-	private Deck[] decks;
-	private int curDeck = 0;
 	
 	public DeckStack(int numDecks) {
-		decks = new Deck[numDecks];
-		for(int i=0; i<decks.length; i++) decks[i] = new Deck();
+		
 	}
 	
 	public Card dealTopCard() {
-		if(decks[curDeck].cardsLeft() > 0) return decks[curDeck].dealTopCard();
 		
-		curDeck++;
-		if(curDeck == decks.length) restoreDecks();
-		
-		return decks[curDeck].dealTopCard();
 	}
 	
 	protected void restoreDecks() {
-		for(Deck d : decks) d.restockDeck();
-		curDeck = 0;
+		
 	}
 	
 	public int cardsLeft() {
-		int c = 0;
-		for(Deck d : decks) c += d.cardsLeft();
-		return c;
+		
 	}
 	
 	
